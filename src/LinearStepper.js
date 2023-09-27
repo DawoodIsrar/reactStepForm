3b58c81a09a97db6703f4d376c3bfd1399b0fb46
DawoodIsrar <85024302+DawoodIsrar@users.noreply.github.com>
2023-09-27 12:54:16 +0500

    stlying added to the form for steller collage project

--- src/LinearStepper.js
+++ src/LinearStepper.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './stlye.css'; // Imp
 import {
   Typography,
   TextField,
@@ -8,6 +10,7 @@ import {
   StepLabel,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { Height } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -20,7 +23,8 @@ function getSteps() {
     "Step 1",
     "Step 2",
     "Step 3",
-    "Step 4"
+    "Step 4",
+    "Step 5"
   ];
 }
 
@@ -34,7 +38,7 @@ function LinearStepper() {
     living_arrangement: "",
     martial: "",
     children: "",
-    numberinfamily: "",
+    aggreement: "",
     numberincollage: "",
     household: "",
   });
@@ -75,7 +79,7 @@ function LinearStepper() {
       living_arrangement: "",
       martial: "",
       children: "",
-      numberinfamily: "",
+      aggreement:"",
       numberincollage: "",
       household: "",
     });
@@ -113,6 +117,7 @@ function LinearStepper() {
 
   return (
     <div>
+      <p>dawood</p>
       <Stepper alternativeLabel activeStep={activeStep}>
         {steps.map((step, index) => {
           const labelProps = {};
@@ -124,7 +129,7 @@ function LinearStepper() {
                 align="center"
                 style={{ display: "block" }}
               >
-                
+
               </Typography>
             );
           }
@@ -189,46 +194,207 @@ function getStepContent(step, formData, handleChange) {
     case 0:
       return (
         <>
-          <TextField
-            id="financialaid"
-            label="Financial aid"
-            variant="outlined"
-            placeholder="Do you plan to apply for financial aid?"
-            fullWidth
-            margin="normal"
-            name="financialaid"
-            value={formData.financialaid}
-            onChange={handleChange}
-          />
-          <TextField
-            id="age"
-            label="Age"
-            variant="outlined"
-            placeholder="How old are you?"
-            fullWidth
-            margin="normal"
-            name="age"
-            value={formData.age}
-            onChange={handleChange}
-          />
-          <TextField
-            id="living_arrangement"
-            label="Living-Arrangement"
-            variant="outlined"
-            placeholder="Where do you plan to live while attending this institution?"
-            fullWidth
-            margin="normal"
-            name="living_arrangement"
-            value={formData.living_arrangement}
-            onChange={handleChange}
-          />
+          <div class="main">
+            <div class="leftt"><i class="bi bi-record-circle"></i><span class="bannerhead "><h2>Please Read</h2></span></div>
+            <div class="rightt"><i class="bi bi-check-circle-fill"></i></div>
+          </div>
+          <div>
+            <ul>
+              <li>The Stellar Career College’s net price calculator is intended to compute the estimated price inclusive of tuition and required fees, books and supplies, room and board (meals), and other related expenses.</li>
+              <li>The calculator does not include estimated grant and scholarship aid</li>
+              <li>The calculator presents current and prospective students, as well as their families, with results somewhat equivalent to what similar students paid in the previous year</li>
+              <li>The estimates provided by this calculator shall not be challenged against the Secretary of Education, the Institution of Higher Education (IHE), or the State</li>
+            </ul>
+          </div>
+          <div>
+            <p>
+              By ticking the box below, I acknowledge that the estimated cost quantified by this net calculator does not represent a final determination of actual price, definite award allocation, or financial assistance value. Rather, it provides an approximate fee inclusive of the cost of attendance and financial aid provided to students in previous years.
+            </p>
+            <input type="checkbox" id="vehicle1" name="aggreement"  required onChange={handleChange}></input>
+<label for="vehicle1"> I Agree</label><br></br>
+          </div>
+          <div class="main">
+            <div class="leftt"><i class="bi bi-record-circle"></i><span class="bannerhead "><h2>Note</h2></span></div>
+            <div class="rightt"><i class="bi bi-check-circle-fill"></i></div>
+          </div>
+          <p>Any information you provide on this site is confidential. The Net Price Calculator does not store your responses or ask for any kind of personal identification information.</p>
+          {/* <p className="npc-section-description">
+            Welcome to Stellar Career College's net price calculator. Begin by
+            reading and agreeing to the statement below. Then follow the
+            instructions on the subsequent screens to receive an estimate of how
+            much students similar to you paid to attend Stellar Career College in
+            2020-21.
+          </p>
+          <p
+            style={{
+              textAlign: 'left',
+              fontSize: '16px',
+              border: '1px solid #ccc',
+              padding: '0px 10px 20px 10px',
+              margin: '0px 50px 20px 50px',
+            }}
+          >
+            <strong>Please read:</strong>  This calculator is intended to provide estimated net price
+            information (defined as estimated cost of attendance - including tuition
+            and required fees, books and supplies, room and board (meals), and other
+            related expenses - minus estimated grant and scholarship aid) to current
+            and prospective students and their families based on what similar
+            students paid in a previous year.
+            <br />
+            <br />
+            By clicking below, I acknowledge that the estimate provided using this
+            calculator does not represent a final determination or actual award of
+            financial assistance or a final net price; it is an estimate based on
+            the cost of attendance and financial aid provided to students in a
+            previous year. The cost of attendance and financial aid availability
+            change year to year. The estimates shall not be binding on the Secretary
+            of Education, the institution of higher education, or the State.
+            <br />
+            <br />
+            Students must complete the Free Application for Federal Student Aid
+            (FAFSA) in order to be eligible for and receive an actual financial aid
+            award that includes Federal grant, loan, or work-study assistance. For
+            more information on applying for Federal student aid, go to{' '}
+            <a href="http://studentaid.gov">http://studentaid.gov</a>.
+            <br />
+            <br />
+            <strong>Note:</strong>Any information that you provide on this site is confidential.
+            The Net Price Calculator does not store your responses or ask for
+            personal identifying information of any kind.
+          </p> */}
         </>
+
       );
 
     case 1:
       return (
         <>
-          <TextField
+          <div>
+            <div class="main">
+              <div class="leftt"><i class="bi bi-record-circle"></i><span class="bannerhead "><h2>Financial Aid</h2></span></div>
+              <div class="rightt"><i class="bi bi-check-circle-fill"></i></div>
+            </div>
+            <div className="inputs-section">
+              <h4>Do you plan to apply for financial aid?</h4>
+              <input
+                type="radio"
+                id="financialaid-yes"
+                name="financialaid"
+                value="yes"
+                onChange={handleChange}
+              />
+              <label htmlFor="financialaid-yes">Yes</label>
+
+              <input
+                type="radio"
+                id="financialaid-no"
+                name="financialaid"
+                value="no"
+                onChange={handleChange}
+              />
+              <label htmlFor="financialaid-no">No</label>
+            </div>
+
+            <div class="main">
+              <div class="leftt"><i class="bi bi-record-circle"></i><span class="bannerhead "><h2>Age</h2></span></div>
+              <div class="rightt"><i class="bi bi-check-circle-fill"></i></div>
+            </div>
+            <div className="inputs-section">
+              <h4>How old are you?</h4>
+              <input
+                style={{ border: '1px solid #69727d', height: '40px', borderRadius: "5px" }}
+                type="number"
+                id="age"
+                name="age"
+                placeholder="Age"
+                value={formData.age}
+                onChange={handleChange}
+              />
+
+
+            </div>
+
+            <div class="main">
+              <div class="leftt"><i class="bi bi-record-circle"></i><span class="bannerhead "><h2>Where do you plan to live while attending this institution?</h2></span></div>
+              <div class="rightt"><i class="bi bi-check-circle-fill"></i></div>
+            </div>
+            <div className="inputs-section">
+              <h4>Do you plan to apply for financial aid?</h4>
+              <input
+                type="radio"
+                id="living-own-roommate"
+                name="living_arrangement"
+                value="Living on my own or with a roommate"
+                onChange={handleChange}
+              />
+              <label htmlFor="living-own-roommate">Living on my own or with a roommate</label>
+
+              <input
+                type="radio"
+                id="living-parents-family"
+                name="living_arrangement"
+                value="Living with my parents or other family members"
+                onChange={handleChange}
+              />
+              <label htmlFor="living-parents-family">Living with my parents or other family members</label>
+            </div>
+
+          </div>
+        </>
+      );
+
+    case 2:
+      return (
+        <>
+          <div class="main">
+            <div class="leftt"><i class="bi bi-record-circle"></i><span class="bannerhead "><h2>Marital Status</h2></span></div>
+            <div class="rightt"><i class="bi bi-check-circle-fill"></i></div>
+          </div>
+          <div className="inputs-section">
+            <h4>Are you (the student) married? Answer 'yes' if you are separated but not divorced.</h4>
+            <input
+              type="radio"
+              id="martial-yes"
+              name="martial"
+              value="yes"
+              onChange={handleChange}
+            />
+            <label htmlFor="financialaid-yes">Yes</label>
+
+            <input
+              type="radio"
+              id="martial-no"
+              name="martial"
+              value="no"
+              onChange={handleChange}
+            />
+            <label htmlFor="financialaid-no">No</label>
+          </div>
+          <div class="main">
+            <div class="leftt"><i class="bi bi-record-circle"></i><span class="bannerhead "><h2>Children</h2></span></div>
+            <div class="rightt"><i class="bi bi-check-circle-fill"></i></div>
+          </div>
+          <div className="inputs-section">
+            <h4>Are you (the student) the primary source of financial support for any children?</h4>
+            <input
+              type="radio"
+              id="children-yes"
+              name="children"
+              value="yes"
+              onChange={handleChange}
+            />
+            <label htmlFor="children-yes">Yes</label>
+
+            <input
+              type="radio"
+              id="children-no"
+              name="children"
+              value="no"
+              onChange={handleChange}
+            />
+            <label htmlFor="children-no">No</label>
+          </div>
+          {/* <TextField
             id="martial"
             label="Marital Status"
             variant="outlined"
@@ -249,7 +415,7 @@ function getStepContent(step, formData, handleChange) {
             name="children"
             value={formData.children}
             onChange={handleChange}
-          />
+          /> */}
           {/* <TextField
             id="alternate-phone"
             label="Alternate Phone"
@@ -263,10 +429,118 @@ function getStepContent(step, formData, handleChange) {
           /> */}
         </>
       );
-    case 2:
+    case 3:
       return (
         <>
-          <TextField
+          <p className="npc-section-description-2">
+            An independent student is one who is at least 24 years old, married, and/or has legal dependents other than a spouse (e.g., children). A student who does not meet any of the above criteria is considered dependent.
+
+
+          </p>
+          <p className="npc-section-description-2">Please provide the following information and then click Continue.</p>
+          <div class="main">
+            <div class="leftt"><i class="bi bi-record-circle"></i><span class="bannerhead "><h2>Number in Family</h2></span></div>
+            <div class="rightt"><i class="bi bi-check-circle-fill"></i></div>
+          </div>
+          <div className="inputs-section">
+            <h4>How many people are in your family's household?</h4>
+            <label>Count yourself, your spouse (if applicable), and any dependent children.
+            </label><br></br>
+            <select id="people" onChange={handleChange} name="familymember" required="required" aria-required="true">
+              <option value="">select</option>
+              <option value="Two people" onChange={handleChange} name='numberinfamily'>Two people</option>
+              <option name='numberinfamily' value="Three people" onChange={handleChange}>Three people</option>
+              <option name='numberinfamily' value="Four people" onChange={handleChange}>Four people</option>
+              <option name='numberinfamily' value="Five people" onChange={handleChange}>Five people</option>
+              <option name='numberinfamily' value="Six people or  more" onChange={handleChange}>Six people or  more</option>
+
+            </select> <br></br>
+
+            {/* <input
+              type="radio"
+              id="children-yes"
+              name="children"
+              value="yes"
+              onChange={handleChange}
+            />
+            <label htmlFor="children-yes">Yes</label>
+
+            <input
+              type="radio"
+              id="children-no"
+              name="children"
+              value="no"
+              onChange={handleChange}
+            />
+            <label htmlFor="children-no">No</label> */}
+          </div>
+          <div class="main">
+            <div class="leftt"><i class="bi bi-record-circle"></i><span class="bannerhead "><h2>Number in College</h2></span></div>
+            <div class="rightt"><i class="bi bi-check-circle-fill"></i></div>
+          </div>
+          <div className="inputs-section">
+            <h4>Of the number in your family above, how many will be in college next year?</h4>
+            <label>Count yourself, your spouse (if applicable), and any dependent children.</label><br></br>
+            <input
+              type="radio"
+              id="numberincollage-yes"
+              name="numberincollage"
+              value="yes"
+              onChange={handleChange}
+            />
+            <label htmlFor="numberincollage-yes">Yes</label>
+
+            <input
+              type="radio"
+              id="numberincollage-no"
+              name="numberincollage"
+              value="no"
+              onChange={handleChange}
+            />
+            <label htmlFor="numberincollage-no">No</label>
+          </div>
+          <div class="main">
+            <div class="leftt"><i class="bi bi-record-circle"></i><span class="bannerhead "><h2>Household Income</h2></span></div>
+            <div class="rightt"><i class="bi bi-check-circle-fill"></i></div>
+          </div>
+          <div className="inputs-section">
+            <h4>What is your annual household income after taxes?</h4>
+            <label>Include income from work, child support, and other sources. If you are married, include your spouse's income.
+
+            </label><br></br>
+            <select onChange={handleChange} id="people" name="household" required="required" aria-required="true">
+              <option value="">Select</option>
+              <option value="Below $30,000" >Below $30,000</option>
+              onChange={handleChange}
+              <option onChange={handleChange} name="household" value="Between $30,000-$39,999">Between $30,000-$39,999</option>
+              <option onChange={handleChange} name="household" value="Between $40,000-$49,999">Between $40,000-$49,999</option>
+              <option onChange={handleChange} name="household" value="Between $60,000-$69,999">Between $60,000-$69,999</option>
+              <option onChange={handleChange} name="household" value="Between $70,000-$79,999">Between $70,000-$79,999</option>
+              <option onChange={handleChange} name="household" value="Between $80,000-$89,999">Between $80,000-$89,999</option>
+              <option onChange={handleChange} name="household" value="Above $99,999">Above $99,999</option>
+
+            </select>
+            <br></br>
+
+            {/* <input
+              type="radio"
+              id="children-yes"
+              name="children"
+              value="yes"
+              onChange={handleChange}
+            />
+            <label htmlFor="children-yes">Yes</label>
+
+            <input
+              type="radio"
+              id="children-no"
+              name="children"
+              value="no"
+              onChange={handleChange}
+            />
+            <label htmlFor="children-no">No</label> */}
+          </div>
+          {/* <TextField
             id="numberinfamily"
             label="Number in Family"
             variant="outlined"
@@ -299,20 +573,20 @@ If you are married, include your spouse's income."
             name="household"
             value={formData.household}
             onChange={handleChange}
-          />
+          /> */}
         </>
       );
-      case 3:
-        return (
-          <>
-            <Typography variant="h6">Collected Data:</Typography>
-            {Object.entries(formData).map(([key, value]) => (
-              <Typography key={key}>
-                {key.replace(/_/g, " ")}: {value}
-              </Typography>
-            ))}
-          </>
-        );
+    case 4:
+      return (
+        <>
+          <Typography variant="h6">Collected Data:</Typography>
+          {Object.entries(formData).map(([key, value]) => (
+            <Typography key={key}>
+              {key.replace(/_/g, " ")}: {value}
+            </Typography>
+          ))}
+        </>
+      );
     default:
       return "unknown step";
   }
